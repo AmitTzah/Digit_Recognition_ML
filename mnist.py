194f097621e0c6997482c3816371c7a8e3d3119c
amittzah <amitq1324@gmail.com>
2023-05-28 14:16:01 +0300

    Add bias term to feature vectors

--- mnist.py
+++ mnist.py
@@ -4,6 +4,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils import check_random_state
 import numpy as np
 
+# script for preprocessing the mnist data and saving it to disk
+
 mnist = fetch_openml('mnist_784')
 X = mnist['data'].astype('float64').to_numpy()
 t = mnist['target'].astype('int').to_numpy()
@@ -11,10 +13,16 @@ random_state = check_random_state(1)
 permutation = random_state.permutation(X.shape[0])
 X = X[permutation]
 t = t[permutation]
-X = X.reshape((X.shape[0], -1)) #This line flattens the image into a vector of size 784
+
+# This line flattens the image into a vector of size 784
+X = X.reshape((X.shape[0], -1))
+
+# Add a column of ones for the bias term
+X = np.hstack((X, np.ones((X.shape[0], 1))))
+
 X_train, X_test, t_train, t_test = train_test_split(X, t, test_size=0.2)
 # The next lines standardize the images
-scaler = StandardScaler() 
+scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
